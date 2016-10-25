acdc6bd9f47d30e68c7b64a76cb422f699177540
Asep Bagja Priandana <bepitulaz@gmail.com>
2016-10-25 18:28:11 +0700

    Adding new controllers

---
+++ controllers/login.js
@@ -0,0 +1,7 @@
+module.exports = {
+    userLogin: userLogin
+};
+
+function userLogin(username, password) {
+    console.log("username is " + username + " and password is " + password);
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var user = require('./controllers/login');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 // setup template engine
@@ -9,6 +12,10 @@ app.set('view engine', '.hbs');
 // serving static files
 app.use(express.static('public'));
 
+// parse post body
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.get('/', function(req, res) {
     res.render('index');
 });
@@ -16,6 +23,10 @@ app.get('/', function(req, res) {
 app.get('/login', function(req, res) {
     res.render('login');
 });
+
+app.post('/login', function(req, res) {
+    user.userLogin(req.body.username, req.body.password);
+})
 
 var listener = app.listen(3000, function() {
     console.log('Example app listening on port ' + listener.address().port);
